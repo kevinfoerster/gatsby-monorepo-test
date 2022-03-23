ec36c7ddb63efced2231a6980e385d03b97d0327
Kevin Förster <kevin@kevinfoerster.com>
2022-03-23 15:35:26 +0100

    initial commit

---
+++ packages/gatsby/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import Button from "@kf/theme"
+
+const index = () => <div>hello from page
+    <Button/>
+</div>
+
+export default index
---
+++ packages/theme/src/components/Button/index.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Button = () => <button>i dont do anything</button>
+
+export default Button
